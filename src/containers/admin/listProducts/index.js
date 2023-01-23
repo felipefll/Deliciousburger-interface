96df156e8fcf0cf664a4cedb840366eb4fde2c92
felipefll <felipeferreiralopeslima@gmail.com>
2023-01-23 11:14:13 -0300

    Organization of the active link in the Admin menu

--- src/containers/admin/listProducts/index.js
+++ src/containers/admin/listProducts/index.js
@@ -14,7 +14,7 @@ import formatCurrency from '../../../utils/formatCurrency'
 import { Container, Img, EditIconStyles } from './styles'
 
 function ListProducts () {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState()
 
   useEffect(() => {
     async function loadOrders () {
@@ -34,39 +34,40 @@ function ListProducts () {
   }
   return (
     <Container>
-       <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Nome</TableCell>
-            <TableCell>Preço</TableCell>
-            <TableCell align='center'>Produto em oferta</TableCell>
-            <TableCell align='center'>Imagem do produto</TableCell>
-            <TableCell>Editar</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {products.map((product) => (
-            <TableRow
-              key={product.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {product.name}
-              </TableCell>
-              <TableCell>{formatCurrency(product.price)}</TableCell>
-              <TableCell align='center'>{isOffer(product.offer)}</TableCell>
-              <TableCell align='center'>
-                <Img src={product.url} alt='imagem-produto' />
-              </TableCell>
-              <TableCell>
-                <EditIconStyles />
-                </TableCell>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell>Preço</TableCell>
+              <TableCell align='center'>Produto em oferta</TableCell>
+              <TableCell align='center'>Imagem do produto</TableCell>
+              <TableCell>Editar</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {products &&
+              products.map((product) => (
+                <TableRow
+                  key={product.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {product.name}
+                  </TableCell>
+                  <TableCell>{formatCurrency(product.price)}</TableCell>
+                  <TableCell align='center'>{isOffer(product.offer)}</TableCell>
+                  <TableCell align='center'>
+                    <Img src={product.url} alt='imagem-produto' />
+                  </TableCell>
+                  <TableCell>
+                    <EditIconStyles />
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   )
 }
