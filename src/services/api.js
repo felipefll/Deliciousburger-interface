fdfe2a55baede9830e236bcfb0cd2a50f111bf0a
felipefll <felipeferreiralopeslima@gmail.com>
2023-09-14 15:50:12 -0300

    fix baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
-import axios from 'axios'
+import axios from "axios";
 
 const apiDeliciousBurger = axios.create({
-  baseURL: 'http://localhost:3000'
-})
+  baseURL: "https://deliciousburger-backend-production.up.railway.app/",
+});
 
-apiDeliciousBurger.interceptors.request.use(async config => {
-  const userData = await localStorage.getItem('deliciousburger:userData')
-  const token = userData && JSON.parse(userData).token
-  config.headers.authorization = `Bearer ${token}`
-  return config
-})
+apiDeliciousBurger.interceptors.request.use(async (config) => {
+  const userData = await localStorage.getItem("deliciousburger:userData");
+  const token = userData && JSON.parse(userData).token;
+  config.headers.authorization = `Bearer ${token}`;
+  return config;
+});
 
-export default apiDeliciousBurger
+export default apiDeliciousBurger;
